fc1c42d48367b14ede8034b4223e6e5325ee452f
abhinavtomar2912-dot <abhinavtomar2912@gmail.com>
2026-02-25 16:19:38 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,31 @@
-
+pipeline {
+    agent any
+    stages {
+        stage('Checkout'){
+            steps {
+                git branch: 'main'
+                    url: 'https://github.com/abhinavtomar2912-dot/devops-1.git'
+            }
+        }
+        stage ('plan'){
+            steps{
+                sh '''
+                terraform init
+                terraform plan
+                '''
+            }
+        }
+        stage ('approve'){
+            steps {
+                input 'Do you want to proceed with Terraform apply?'
+            }
+        }
+        stage ('apply'){
+            steps {
+                sh '''
+                terraform apply --auto-approve
+                '''
+            }
+        }
+    }
+}
